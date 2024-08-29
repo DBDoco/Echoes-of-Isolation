9e99ac468be825fd24d0b10c664fc7bc72f50c84
DBDoco <dbdoco@gmail.com>
2024-08-29 11:17:29 +0200

    Finish second level

--- Assets/Scripts/DocumentInteraction.cs
+++ Assets/Scripts/DocumentInteraction.cs
@@ -1,60 +1,79 @@
 using TMPro;
 using UnityEngine;
+using System.Collections;
 
 public class DocumentInteraction : MonoBehaviour
 {
-    public float Distance = PlayerCasting.DistanceFromTarget; // Assuming PlayerCasting script provides the distance
-    public GameObject documentUI; // The UI element that displays the document content
-    public AudioSource paperPickupSound; // The sound to play when picking up the document
-
-    public TextMeshProUGUI interactionText; // The TextMeshProUGUI for interaction text
-
+    public float Distance = PlayerCasting.DistanceFromTarget;
+    public GameObject documentUI;
+    public AudioSource paperPickupSound;
+    public AudioSource playerVoice;
+    public TextMeshProUGUI interactionText;
+    public GameObject objectiveComplete;
     private bool isDocumentPickedUp = false;
+    private bool hasPlayedVoice = false;
 
     void Start()
     {
-        paperPickupSound.playOnAwake = false; // Ensure the sound doesn't play on start
-        documentUI.SetActive(false); // Ensure the document UI is hidden at start
+        paperPickupSound.playOnAwake = false;
+        playerVoice.playOnAwake = false;
+        documentUI.SetActive(false);
     }
 
     void Update()
     {
-        Distance = PlayerCasting.DistanceFromTarget; // Update distance from player to object
+        Distance = PlayerCasting.DistanceFromTarget;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (isDocumentPickedUp)
+            {
+                CloseDocument();
+            }
+            else if (Distance <= 2)
+            {
+                PickupDocument();
+            }
+        }
     }
 
     void PickupDocument()
     {
-        paperPickupSound.Play(); // Play the paper pickup sound
-        isDocumentPickedUp = true; // Mark the document as picked up
-        documentUI.SetActive(true); // Show the document UI
-        interactionText.enabled = false; // Hide the interaction text
+        paperPickupSound.Play();
+        objectiveComplete.SetActive(true);
+        isDocumentPickedUp = true;
+        documentUI.SetActive(true);
+        interactionText.enabled = false;
+
+        if (!hasPlayedVoice)
+        {
+            StartCoroutine(PlayVoiceWithDelay());
+            hasPlayedVoice = true;
+        }
+    }
+
+    IEnumerator PlayVoiceWithDelay()
+    {
+        yield return new WaitForSeconds(3f);
+        playerVoice.Play();
     }
 
     void OnMouseOver()
     {
         if (!isDocumentPickedUp && Distance <= 2)
         {
-            interactionText.text = "[E] Pick up document"; // Set the interaction text
-            interactionText.enabled = true; // Show the interaction text
-
-            if (Input.GetButtonDown("Action"))
-            {
-                if (Distance <= 2)
-                {
-                    PickupDocument(); // Call the function to pick up the document
-                }
-            }
+            interactionText.text = "[E] Pick up document";
+            interactionText.enabled = true;
         }
     }
 
     void OnMouseExit()
     {
-        interactionText.enabled = false; // Hide the interaction text when not hovering
+        interactionText.enabled = false;
     }
 
     public void CloseDocument()
     {
-        documentUI.SetActive(false); // Hide the document UI when closing
-        isDocumentPickedUp = false; // Allow document interaction again if needed
+        documentUI.SetActive(false);
+        isDocumentPickedUp = false;
     }
 }
